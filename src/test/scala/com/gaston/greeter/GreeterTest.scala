fc818a52edd8450e3841259258730875341cf8e0
Gastón Schabas <gastonschabas@gmail.com>
2020-07-06 20:57:25 -0300

    add some code with tests to see sonar in action

---
+++ src/test/scala/com/gaston/greeter/GreeterTest.scala
@@ -0,0 +1,13 @@
+package com.gaston.greeter
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class GreeterTest extends AnyFunSuite with Matchers {
+
+  test("returns hello $name") {
+    val greeter = new Greeter
+    greeter.sayHelloTo("gaston") should be("hello gaston")
+  }
+
+}
