f1b01c0e53acae95d5d7976710ca4df98a196b58
Gastón Schabas <gastonschabas@gmail.com>
2020-08-22 17:19:27 -0300

    formatted applied

--- src/main/scala/com/gaston/controller/HelloController.scala
+++ src/main/scala/com/gaston/controller/HelloController.scala
@@ -1,12 +1,19 @@
 package com.gaston.controller
 
 import javax.inject.Inject
-import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
+import play.api.mvc.{
+  AbstractController,
+  Action,
+  AnyContent,
+  ControllerComponents
+}
 
-class HelloController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HelloController @Inject() (cc: ControllerComponents)
+    extends AbstractController(cc) {
 
-  def index: Action[AnyContent] = Action { _ =>
-    Ok("hello world")
-  }
+  def index: Action[AnyContent] =
+    Action { _ =>
+      Ok("hello world")
+    }
 
 }
