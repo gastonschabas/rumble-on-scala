a7285bc9e536867bede2242fdf8d76d5b049f676
Gastón Schabas <gastonschabas@gmail.com>
2020-08-31 01:41:11 -0300

    hello repository with it test using testcontainer (#28)
    * removed Adder dummy class
    
    * added play-slick, testcontainer-scala and random-data-generator as dependencies
    
    * integration tests added to CI/CD pipeline
    
    * hello repository added with corresponding integration tests

---
+++ src/it/scala/com/gaston/repository/HelloRepositoryItTest.scala
@@ -0,0 +1,78 @@
+package com.gaston.repository
+
+import com.danielasfregola.randomdatagenerator.RandomDataGenerator._
+import com.dimafeng.testcontainers.{ForAllTestContainer, PostgreSQLContainer}
+import com.gaston.model.Hello
+import org.scalacheck.{Arbitrary, Gen}
+import org.scalatest.flatspec.AsyncFlatSpec
+import org.scalatest.matchers.should.Matchers
+import play.api.db.slick.DatabaseConfigProvider
+import play.api.inject.guice.GuiceApplicationBuilder
+
+class HelloRepositoryItTest
+    extends AsyncFlatSpec
+    with Matchers
+    with ForAllTestContainer {
+
+  override lazy val container: PostgreSQLContainer = PostgreSQLContainer()
+
+  lazy val app = GuiceApplicationBuilder()
+    .configure(
+      "slick.dbs.default.profile" -> "slick.jdbc.PostgresProfile$",
+      "slick.dbs.default.db.driver" -> container.driverClassName,
+      "slick.dbs.default.db.url" -> container.jdbcUrl,
+      "slick.dbs.default.db.user" -> container.username,
+      "slick.dbs.default.db.password" -> container.password
+    )
+    .build()
+
+  lazy val dbConfigProvider: DatabaseConfigProvider =
+    app.injector.instanceOf[DatabaseConfigProvider]
+
+  lazy val repo = new HelloRepository(dbConfigProvider)
+
+  lazy val hellosSchemaCreated = repo.createTable
+
+  case class Language(code: String)
+  implicit val arbitraryLang: Arbitrary[Language] = Arbitrary {
+    for {
+      x <- Gen.listOfN(2, Gen.alphaLowerChar).map(_.mkString)
+    } yield Language(x)
+  }
+
+  val helloSize = 15
+  lazy val randomHellos: Seq[Hello] = random[Language](helloSize).distinct
+    .map(l => Hello(None, random[String], l.code))
+
+  lazy val randomHelloNotLoaded: Hello = randomHellos.head
+  lazy val randomHellosForInitialLoading: Seq[Hello] = randomHellos.tail
+
+  implicit lazy val arbitraryString: Arbitrary[String] = Arbitrary(Gen.alphaStr)
+
+  lazy val hellosInitialDataLoaded = for {
+    _ <- hellosSchemaCreated
+    _ <- repo.save(randomHellosForInitialLoading)
+  } yield {}
+
+  behavior of s"Langs loaded in db: ${randomHellosForInitialLoading.map(_.lang).mkString(", ")}"
+  randomHellosForInitialLoading.foreach { randomHello =>
+    it should s"found language ${randomHello.lang}" in {
+      for {
+        _ <- hellosInitialDataLoaded
+        maybeHello <- repo.find(randomHello.lang)
+      } yield {
+        maybeHello.isDefined should be(true)
+      }
+    }
+  }
+
+  behavior of s"Lang not loaded in db: ${randomHelloNotLoaded.lang}"
+  it should s"not found language ${randomHelloNotLoaded.lang} " in {
+    for {
+      _ <- hellosInitialDataLoaded
+      maybeHello <- repo.find(randomHelloNotLoaded.lang)
+    } yield {
+      maybeHello.isDefined should be(false)
+    }
+  }
+}
---
+++ src/main/scala/com/gaston/model/Hello.scala
@@ -0,0 +1,3 @@
+package com.gaston.model
+
+final case class Hello(id: Option[Long], msg: String, lang: String)
